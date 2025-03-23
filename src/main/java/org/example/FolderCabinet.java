fd7bdcba103ec182657b1b724f738ea19b66b853
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2025-03-23 12:24:30 +0100

    Implement Cabinet interface in FolderCabinet class
    Add collectAllFolders method to process Folder and MultiFolder interfaces, allowing recursive traversal of nested structures.

--- src/main/java/org/example/FolderCabinet.java
+++ src/main/java/org/example/FolderCabinet.java
@@ -1,8 +1,48 @@
 package org.example;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
 
-public class FolderCabinet {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@AllArgsConstructor
+public class FolderCabinet implements Cabinet {
 
     private List<Folder> folders;
+
+    @Override
+    public Optional<Folder> findFolderByName(String name) {
+        return folders.stream()
+                .flatMap(this::collectAllFolders)
+                .filter(folder -> name.equals(folder.getName()))
+                .findAny();
+    }
+
+    @Override
+    public List<Folder> findFoldersBySize(String size) {
+        return folders.stream()
+                .flatMap(this::collectAllFolders)
+                .filter(folder -> size.equals(folder.getSize()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public int count() {
+        return (int) folders.stream()
+                .flatMap(this::collectAllFolders)
+                .count();
+    }
+
+    private Stream<Folder> collectAllFolders(Folder folder) {
+        return Stream.concat(
+                Stream.of(folder),
+                Optional.ofNullable(folder)
+                        .filter(MultiFolder.class::isInstance)
+                        .map(MultiFolder.class::cast)
+                        .stream()
+                        .flatMap(multiFolder -> multiFolder.getFolders().stream().flatMap(this::collectAllFolders))
+        );
+    }
 }
